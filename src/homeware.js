9f6f7b8c6b1a32c3164d602a27639ee3ab5c972a
Irina Vasilescu <irinaavasilescu@gmail.com>
2025-11-10 14:24:43 +0200

    converted more pngs to webp

--- src/homeware.js
+++ src/homeware.js
@@ -1,13 +1,13 @@
-import x_basket from './homeware/x_basket.png';
-import y_basket from './homeware/y_basket.png';
-import z_basket from './homeware/z_basket.png';
-import a_coaster from './homeware/a_coaster.png';
-import b_coaster from './homeware/b_coaster.png';
-import c_coaster from './homeware/c_coaster.png';
-import d_coaster from './homeware/d_coaster.png';
-import e_coaster from './homeware/e_coaster.png';
-import f_coaster from './homeware/f_coaster.png';
-import g_doily from './homeware/g_doily.png';
+import x_basket from './homeware/x_basket.webp';
+import y_basket from './homeware/y_basket.webp';
+import z_basket from './homeware/z_basket.webp';
+import a_coaster from './homeware/a_coaster.webp';
+import b_coaster from './homeware/b_coaster.webp';
+import c_coaster from './homeware/c_coaster.webp';
+import d_coaster from './homeware/d_coaster.webp';
+import e_coaster from './homeware/e_coaster.webp';
+import f_coaster from './homeware/f_coaster.webp';
+import g_doily from './homeware/g_doily.webp';
 
 export const homeware = [
     {
