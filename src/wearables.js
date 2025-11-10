0834eb3130248b5b54fe87b219e47d4e9bfe9cf8
Irina Vasilescu <irinaavasilescu@gmail.com>
2025-11-10 14:35:43 +0200

    converted more pngs to webp

--- src/wearables.js
+++ src/wearables.js
@@ -1,19 +1,18 @@
-import a_top from './wearables/a_top.png';
-import a_shawl from './wearables/a_shawl.png';
-import ab_shophie from './wearables/ab_shophie.png';
-import ac_sophie from './wearables/ac_sophie.png';
-import b_top from './wearables/b_top.png';
-import c_top from './wearables/c_top.png';
-import a_hat from './wearables/a_hat.png';
-import b_hat from './wearables/b_hat.png';
-import c_hat from './wearables/c_hat.png';
-import d_hat from './wearables/d_hat.png';
-import e_hat from './wearables/e_hat.png';
-import f_hat from './wearables/f_hat.png';
-import g_bag from './wearables/g_bag.png';
-import h_bag from './wearables/h_bag.png';
-import i_pouch from './wearables/i_pouch.png';
-import j_bag from './wearables/j_bag.png';
+import a_top from './wearables/a_top.webp';
+import a_shawl from './wearables/a_shawl.webp';
+import ab_shophie from './wearables/ab_shophie.webp';
+import ac_sophie from './wearables/ac_sophie.webp';
+import b_top from './wearables/b_top.webp';
+import c_top from './wearables/c_top.webp';
+import a_hat from './wearables/a_hat.webp';
+import b_hat from './wearables/b_hat.webp';
+import c_hat from './wearables/c_hat.webp';
+import d_hat from './wearables/d_hat.webp';
+import e_hat from './wearables/e_hat.webp';
+import g_bag from './wearables/g_bag.webp';
+import h_bag from './wearables/h_bag.webp';
+import i_pouch from './wearables/i_pouch.webp';
+import j_bag from './wearables/j_bag.webp';
 
 export const wearables = [
     {
