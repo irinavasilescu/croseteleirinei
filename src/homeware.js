c04cfba80fbeadd8cd959f067846bdb064e14dff
Irina Vasilescu <irinaavasilescu@gmail.com>
2025-11-13 13:20:00 +0200

    new hat

--- src/homeware.js
+++ src/homeware.js
@@ -1,13 +1,6 @@
 import x_basket from './homeware/x_basket.webp';
 import y_basket from './homeware/y_basket.webp';
 import z_basket from './homeware/z_basket.webp';
-import a_coaster from './homeware/a_coaster.webp';
-import b_coaster from './homeware/b_coaster.webp';
-import c_coaster from './homeware/c_coaster.webp';
-import d_coaster from './homeware/d_coaster.webp';
-import e_coaster from './homeware/e_coaster.webp';
-import f_coaster from './homeware/f_coaster.webp';
-import g_doily from './homeware/g_doily.webp';
 
 export const homeware = [
     {
--- src/wearables.js
+++ src/wearables.js
@@ -1,28 +1,19 @@
-import a_top from './wearables/a_top.webp';
-import a_shawl from './wearables/a_shawl.webp';
-import ab_shophie from './wearables/ab_shophie.webp';
-import ac_sophie from './wearables/ac_sophie.webp';
-import b_top from './wearables/b_top.webp';
-import c_top from './wearables/c_top.webp';
 import a_hat from './wearables/a_hat.webp';
 import b_hat from './wearables/b_hat.webp';
 import c_hat from './wearables/c_hat.webp';
 import d_hat from './wearables/d_hat.webp';
 import e_hat from './wearables/e_hat.webp';
-import g_bag from './wearables/g_bag.webp';
-import h_bag from './wearables/h_bag.webp';
-import i_pouch from './wearables/i_pouch.webp';
-import j_bag from './wearables/j_bag.webp';
+import f_hat from './wearables/f_hat.webp';
 
 export const wearables = [
     {
         img: a_hat,
         name: 'Căciulă',
         price: null,
-        description: "Căciulă croșetată, realizată din fir 100% acril",
+        description: "Căciulă croșetată, realizată din multiple culori de fire, 100% acril.",
         id: 'a_hat',
         tags: ['wearables'],
-        in_stock: false,
+        in_stock: true,
     },
     {
         img: b_hat,
@@ -57,6 +48,15 @@ export const wearables = [
         price: null,
         description: "Căciulă croșetată, realizată din fir 100% acril",
         id: 'e_hat',
+        tags: ['wearables'],
+        in_stock: false,
+    },
+    {
+        img: f_hat,
+        name: 'Căciulă',
+        price: null,
+        description: "Căciulă croșetată, realizată din fir 100% acril",
+        id: 'f_hat',
         tags: ['wearables'],
         in_stock: false,
     },
