080c88feb3ce59ad81c35c1d3fa43606b8006262
Irina Vasilescu <irinaavasilescu@gmail.com>
2025-12-04 23:59:37 +0200

    add dolly to nav

--- src/App.js
+++ src/App.js
@@ -659,6 +659,7 @@ function App() {
           <nav className="nav nav-tabs">
             {[
               { to: ROUTES.home, key: 'home', label: 'Home', end: true },
+              { to: ROUTES.dolls, key: 'dolls', label: 'Dolls' },
               { to: ROUTES.products, key: 'products', label: 'Products' },
               { to: ROUTES.contact, key: 'contact', label: 'Contact' },
             ].map((link) => {
